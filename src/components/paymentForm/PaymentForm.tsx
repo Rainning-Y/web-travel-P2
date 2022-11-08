a5d79f72f14af23ba726dfcb56ceb8cb6f41db63
Rainning-Y <g1545384595@gmail.com>
2022-11-08 15:38:11 +0800

    P1

---
+++ src/components/paymentForm/PaymentForm.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+import { Input, Card } from "antd";
+import styles from "./PaymentForm.module.css";
+
+export const PaymentForm = () => {
+
+
+  return (
+    <Card
+      title="信用卡"
+      bordered={false}
+      
+    >
+     
+    </Card>
+  );
+};
